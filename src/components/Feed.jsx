e8bc5280fd4c2658fb403b1b2a9238cc8e2a5914
viplove3541 <viplovekale@gmail.com>
2023-04-27 18:55:45 +0530

    setup gh-pages

---
+++ src/components/Feed.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { useState, useEffect } from 'react';
+import { Stack, Box, Typography } from '@mui/material'
+import SideBar from './SideBar';
+import Video from './Video';
+import { fetchFromAPI } from '../utilities/fetchFromAPI';
+
+const Feed = () => {
+  const [selectedCategory, setSelectedCategory] = useState("New");
+  const [videos, setVideos] = useState(null);
+
+  useEffect(() => {
+    setVideos(null);
+
+    fetchFromAPI(`search?part=snippet&q=${selectedCategory}`)
+      .then((data) => setVideos(data.items))
+    }, [selectedCategory]);
+
+  return (
+    <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
+      <Box sx={{ height: { sx: "auto", md: "92vh" }, borderRight: "1px solid #3d3d3d", px: { sx: 0, md: 2 } }}>
+        <SideBar selectedCategory={selectedCategory} setSelectedCategory={setSelectedCategory} />
+        
+        <Typography className="copyright" variant="body2" sx={{ mt: 1.5, color: "#fff", }}>
+          Copyright © 2023 Viplove_18
+        </Typography>
+      </Box>
+
+      <Box p={2} sx={{ overflowY: "auto", height: "90vh", flex: 2 }}>
+        <Typography variant="h4" fontWeight="bold" mb={2} sx={{ color: "white" }}>
+          {selectedCategory} <span style={{ color: "#FC1503" }}>videos</span>
+        </Typography>
+
+        <Video videos={videos} />
+      </Box>
+    </Stack>
+  );
+};
+
+export default Feed;
---
+++ src/components/Video.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Stack, Box } from "@mui/material";
+import VideoCard from "./VideoCard";
+import ChannelCard from "./ChannelCard";
+import Loader from "./Loader";
+
+
+
+const Video = ({ videos, direction }) => {
+  if(!videos?.length) return <Loader />;
+  
+  return (
+    <Stack direction={direction || "row"} flexWrap="wrap" justifyContent="start" alignItems="start" gap={2}>
+      {videos.map((item, idx) => (
+        <Box key={idx}>
+          {item.id.videoId && <VideoCard video={item} /> }
+          {item.id.channelId && <ChannelCard channelDetail={item} />}
+        </Box>
+      ))}
+    </Stack>
+  );
+}
+
+export default Video;
